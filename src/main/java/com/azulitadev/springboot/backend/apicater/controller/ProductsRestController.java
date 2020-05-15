7f6c41cfa9c64e4b588812564c210545822d4ad5
sebasnavarro <christiam_navarrro@hotmail.com>
2020-05-14 19:56:38 -0500

    Adaptando

--- src/main/java/com/azulitadev/springboot/backend/apicater/controller/ProductsRestController.java
+++ src/main/java/com/azulitadev/springboot/backend/apicater/controller/ProductsRestController.java
@@ -31,75 +31,74 @@ import io.swagger.annotations.ApiResponses;
 @RequestMapping("/api/products")
 @Api(value = "Article Resource")
 public class ProductsRestController {
-	
+
 	@Autowired
 	private IProductsService productsService;
+
 	@ApiOperation(value = "Return essential items")
-    @ApiResponses(value = { @ApiResponse(code =100, message="100 is the message"),  @ApiResponse(code = 200,message="Successful return essential items"),
-    })
+	@ApiResponses(value = { @ApiResponse(code = 100, message = "100 is the message"),
+			@ApiResponse(code = 200, message = "Successful return essential items"), })
 	@GetMapping
 	public ResponseEntity<?> getAll() {
-	List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-	Map<String, Object> response = null;
-	List<Products> products = productsService.findAll();
-	
-	for (int i=0; i<products.size(); i++) {
-		response = new HashMap<String, Object>();
-		response.put("category", products.get(i).getCategory());
-		response.put("name", products.get(i).getName());
-		response.put("price", products.get(i).getPrices().get(products.get(i).getPrices().size()-1).getPrice());
-		response.put("quantity", products.get(i).getQuantity());
-		response.put("image", products.get(i).getImage());
-		response.put("id", products.get(i).getId());
-		list.add(response);
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Map<String, Object> response = null;
+		List<Products> products = productsService.findAll();
+
+		for (int i = 0; i < products.size(); i++) {
+			response = new HashMap<String, Object>();
+			response.put("category", products.get(i).getCategory());
+			response.put("name", products.get(i).getName());
+			response.put("price", products.get(i).getPrices().get(products.get(i).getPrices().size() - 1).getPrice());
+			response.put("quantity", products.get(i).getQuantity());
+			response.put("image", products.get(i).getImage());
+			response.put("id", products.get(i).getId());
+			list.add(response);
+		}
+		int count = list.size();
+		ProductsDto articuloDto = new ProductsDto();
+		articuloDto.setCount(count);
+		articuloDto.setProducts(list);
+		return new ResponseEntity<ProductsDto>(articuloDto, HttpStatus.OK);
 	}
-	int count = list.size();
-	ProductsDto articuloDto = new ProductsDto(); 
-	articuloDto.setCount(count);
-	articuloDto.setProducts(list);
-	return new ResponseEntity<ProductsDto>(articuloDto, HttpStatus.OK);
+
+	@RequestMapping(value = "/page/{page}", method = RequestMethod.GET, produces = "application/json")
+	public Page<IProductsProjection> getAll(@PathVariable Integer page) {
+
+		return productsService.find(PageRequest.of(page, 6));
 	}
-	
-	@RequestMapping(value = "/page/{page}", method = RequestMethod.GET, produces="application/json")
-	public  Page<IProductsProjection> getAll(@PathVariable Integer page) {
-  
-	return productsService.find(PageRequest.of(page, 6));
-	}
-  
+
 	@GetMapping("/{terminal}")
 	public List<IProductsProjection> getByTerminal(@PathVariable String terminal) {
 
 		return productsService.finByTerminal(terminal);
 	}
 
-	
-	
 	@ApiOperation(value = "Return essential items")
-    @ApiResponses(value = { @ApiResponse(code =100, message="100 is the message"),  @ApiResponse(code = 200,message="Successful return essential items"),
-    })
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces="application/json")
+	@ApiResponses(value = { @ApiResponse(code = 100, message = "100 is the message"),
+			@ApiResponse(code = 200, message = "Successful return essential items"), })
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<?> show(@PathVariable String id) {
 
 		Products producto = null;
 		Map<String, Object> response = new HashMap<>();
-        
+
 		try {
-			producto  = productsService.findById(id);//articuloService.findById(id);
+			producto = productsService.findById(id);// articuloService.findById(id);
 			response.put("category", producto.getCategory());
 			response.put("name", producto.getName().toString());
-			response.put("price", producto.getPrices().get(producto.getPrices().size()-1).getPrice());
+			response.put("price", producto.getPrices().get(producto.getPrices().size() - 1).getPrice());
 			response.put("quantity", producto.getQuantity());
 			response.put("image", producto.getImage());
 			response.put("id", producto.getId().toString());
-			
+
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar la consulta en la base de datos");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 
 	}
-	
+
 }
